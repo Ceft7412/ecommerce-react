f7cdc3503ce9ddaaf83ce9e238b285ffc3c580bf
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-18 07:28:26 +0800

    @update Layout for navigation bar.

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { GoPerson } from "react-icons/go";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { AiOutlineHeart } from "react-icons/ai";
 
 function NavigationBar() {
   return (
-    <div>NavigationBar</div>
-  )
+    <header className="fixed top-0 left-0 right-0 h-[80px] flex justify-between items-center p-6 px-10 bg-transparent">
+      <ul className="flex gap-4 te  xt-[16px] font-medium text-neutral-500">
+        <li>Home</li>
+        <li>Shop</li>
+      </ul>
+      <div className="">
+        <div className="flex text-[25px] gap-3 text-neutral-600">
+          <GoPerson />
+          <AiOutlineHeart />
+          <HiOutlineShoppingBag />
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default NavigationBar
+export default NavigationBar;
