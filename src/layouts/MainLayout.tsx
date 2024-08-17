5858a0ee289dd82ee5c01fcde18f4fbd6ca0ac38
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-18 07:14:00 +0800

    @import Navigation bar component.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import NavigationBar from "../components/NavigationBar";
 
 function MainLayout() {
   return (
     <>
-      <Outlet />
-      <div className="">hey</div>
+      <NavigationBar />
+      <main className="pt-[80px]">
+        <Outlet />
+      </main>
     </>
   );
 }
