b5dfca973a73a2e9da406bd38bb672930e57c2b7
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-19 07:14:24 +0800

    @create Scroll component to track the scrollY and show the scroll to top.

---
+++ src/components/ScrollTop.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { IoChevronUp } from "react-icons/io5";
+
+function ScrollTop() {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTOp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <>
+      {isVisible && (
+        <div
+          className="fixed p-2 text-white right-10 bottom-10 bg-orange-500 cursor-pointer text-[18px]"
+          onClick={scrollToTOp}
+        >
+          <IoChevronUp />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default ScrollTop;
