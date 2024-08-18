83b7e0550bfd5d766c1acc52dd9da679d99c705f
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-18 22:48:21 +0800

    @import Cart component.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
+import Cart from "../components/Cart";
 
 function MainLayout() {
   return (
     <>
       <NavigationBar />
+      <Cart />
       <main className="max-w-screen-2xl mx-auto">
         <Outlet />
       </main>
