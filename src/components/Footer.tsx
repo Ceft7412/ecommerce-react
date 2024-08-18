f87a199a3184799761f598f36017c237b0d15465
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-19 07:01:54 +0800

    @create Footer component.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bottom-0 left-0 right-0 h-20 bg-neutral-200 flex items-center justify-center mt-20">
+      <div className="">
+        <p className="text-neutral-600 text-center font-medium">
+          &copy; 2024 All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
