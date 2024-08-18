d0a3fffca2a9ddc45f0adb0adc0523c8212009c8
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-19 07:14:55 +0800

    @import ScrollTop component in the MainLayout to make it global.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
 import Cart from "../components/Cart";
 import Footer from "../components/Footer";
+import ScrollTop from "../components/ScrollTop";
 
 function MainLayout() {
   return (
@@ -12,6 +13,7 @@ function MainLayout() {
       <main className="max-w-screen-2xl mx-auto">
         <Outlet />
       </main>
+      <ScrollTop />
       <Footer />
     </>
   );
