3da8e97fd3df211904507e970ff4f9b57097ade1
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-18 22:04:09 +0800

    @update Types for different properties.

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { FaStar } from "react-icons/fa";
+import { FaRegStar } from "react-icons/fa6";
+
 type Product = {
   id?: number;
   title?: string;
   image?: string;
+  price?: number;
+  rating?: {
+    rate: number;
+    count: number;
+  };
 };
 function Products() {
   const [products, setProducts] = useState<Product[] | null>([]);
@@ -23,17 +31,32 @@ function Products() {
   return (
     <section className="h-[100%] py-6 px-16 ">
       <h1 className="font-bold text-[24px] mb-10">Products You Might Like</h1>
-      <div className="grid grid-cols-6 gap-16">
+      <div className="grid grid-cols-4 gap-16">
         {products &&
           products.map((product) => (
             <div
-              className="w-56 flex flex-col gap-2 shadow-md rounded-xl"
+              className="p-3 py-6 w-80 flex flex-col gap-2 border rounded-2xl"
               key={product.id}
             >
-              <div className="h-60 w-fullflex justify-center items-center p-6">
+              <div className="h-64 w-[95%] flex justify-center items-center p-6">
                 <img src={product.image} alt={product.title} className="h-full w-full" />
               </div>
-              <span className="p-2">{product.title}</span>
+              <div className="h-56 text-[17px] flex flex-col justify-between">
+                <div className="flex gap-4 flex-col">
+                  <span className="font-medium">{product.title}</span>
+                  <span className="font-medium">${product.price}</span>
+                  <div className="rating flex mb-5">
+                    {[...Array(5)].map((_, i) => (
+                      <FaRegStar key={i} className="" />
+                    ))}
+                  </div>
+                </div>
+                <div className="">
+                  <span className="font-medium rounded-full p-2 border-black border-2 hover:bg-orange-500 hover:border-orange-500 transition-colors duration-400 cursor-default hover:text-white ">
+                    Add to Cart
+                  </span>
+                </div>
+              </div>
             </div>
           ))}
       </div>
