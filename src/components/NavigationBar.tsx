a33b93f657bca9e9038f5ff32d98339543c3f585
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-18 22:47:59 +0800

    @update Toggling the cart.

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 import { GoPerson } from "react-icons/go";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { AiOutlineHeart } from "react-icons/ai";
 import { CgSearch } from "react-icons/cg";
 
 function NavigationBar() {
+  const { toggleCart } = useContext(CartContext);
   return (
     <header className="fixed z-20  bg-white shadow top-0 left-0 right-0 h-[80px] flex justify-between items-center p-6 px-10 bg-transparent">
       <ul className="flex gap-4 text-[16px] font-medium text-neutral-500">
@@ -15,7 +17,7 @@ function NavigationBar() {
         <div className="flex text-[25px] gap-4 text-neutral-600">
           <GoPerson />
           <AiOutlineHeart />
-          <HiOutlineShoppingBag />
+          <HiOutlineShoppingBag onClick={toggleCart} />
         </div>
       </div>
     </header>
