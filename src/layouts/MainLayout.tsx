c4180380625d6a8c5ebf396fb4cc5b198394a861
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-19 13:33:17 +0800

    @update Handle event to close the drop down.

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { DropDownContext } from "../context/DropDownContext";
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
 import Cart from "../components/Cart";
@@ -6,15 +7,19 @@ import Footer from "../components/Footer";
 import ScrollTop from "../components/ScrollTop";
 
 function MainLayout() {
+  const { isDropDownOpen, toggleDropDown, setIsDropDownOpen } =
+    useContext(DropDownContext);
   return (
     <>
-      <NavigationBar />
-      <Cart />
-      <main className="max-w-screen-2xl mx-auto">
-        <Outlet />
-      </main>
-      <ScrollTop />
-      <Footer />
+      <div onClick={() => setIsDropDownOpen(false)}>
+        <NavigationBar />
+        <Cart />
+        <main className="max-w-screen-2xl mx-auto">
+          <Outlet />
+        </main>
+        <ScrollTop />
+        <Footer />
+      </div>
     </>
   );
 }
