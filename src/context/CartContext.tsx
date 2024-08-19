e33d46ea35c184509d588b6169e5948aa558c418
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-20 07:30:07 +0800

    @update Type checking function of setting the cart items and adding a Type of Cart to hold the type of each property in the cart.

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,22 +1,49 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
-const initialState = {
+const initialState: CartContextState = {
   isCartOpen: false,
+  cartItems: null,
+  setCartItems: () => {},
   toggleCart: () => {},
 };
+type CartContextState = {
+  isCartOpen: boolean;
+  cartItems: Cart[] | null;
+  setCartItems: React.Dispatch<React.SetStateAction<Cart[] | null>>;
+  toggleCart: () => void;
+};
 
-interface CartContextType {
+type Cart = {
+  id?: number;
+  title?: string;
+  image?: string;
+  price?: number;
+  rating?: {
+    rate?: number;
+    count?: number;
+  };
+  quantity?: number;
+};
+
+type CartContextType = {
   children: React.ReactNode;
-}
-export const CartContext = createContext(initialState);
+};
+export const CartContext = createContext<CartContextState>(initialState);
 function CartProvider({ children }: CartContextType) {
   const [isCartOpen, setIsCartOpen] = useState<boolean>(false);
+  const [cartItems, setCartItems] = useState<Cart[] | null>(null);
+  console.log("cartItems: ", cartItems);
 
   const toggleCart = () => {
     setIsCartOpen((prev) => !prev);
   };
+
+  useEffect(() => {
+    const cart = JSON.parse(localStorage.getItem("carts") || "[]");
+    setCartItems(cart);
+  }, []);
   return (
-    <CartContext.Provider value={{ isCartOpen, toggleCart }}>
+    <CartContext.Provider value={{ isCartOpen, cartItems, toggleCart, setCartItems }}>
       {children}
     </CartContext.Provider>
   );
