108a30971e411a22a2b09833a641e34c339dd4c6
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-20 07:27:51 +0800

    @update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 function Footer() {
   return (
-    <footer className=" bottom-0 left-0 right-0 h-20 bg-neutral-200 flex items-center justify-center mt-20">
-      <div className="">
+    <footer className="bottom-0 left-0 right-0 h-20 bg-neutral-200 flex items-center justify-center mt-20">
+      <div>
         <p className="text-neutral-600 text-center font-medium">
           &copy; 2024 All rights reserved.
         </p>
