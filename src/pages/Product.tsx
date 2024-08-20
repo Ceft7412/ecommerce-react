55683b4a510b025bc2e871a961af8ad0d926e64c
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-20 08:07:03 +0800

    @update Fetching specific product from the api.

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,7 +1,39 @@
 import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type ProductType = {
+  id?: number;
+  title?: string;
+  image?: string;
+  price?: number;
+  rating?: {
+    rate?: number;
+    count?: number;
+  };
+};
 function Product() {
   const { id } = useParams();
-  return <div>The id: {id}</div>;
+  const [product, setProduct] = useState<ProductType | null>(null);
+
+  // Request the product data from the API
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+      setProduct(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    // Fetch the product when the component mounts
+    fetchProduct();
+  }, []);
+  return (
+    <section className="min-h-screen pt-[80px] px-20">
+      The id: {id} {product?.title}
+    </section>
+  );
 }
 
 export default Product;
